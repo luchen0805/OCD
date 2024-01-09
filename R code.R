cbabe2b62150e67e013eb985806eef9ee00f51c0
余璐辰 <yuluchen@yuluchendeMacBook-Pro-5.local>
2024-01-09 16:19:20 -0600

    add file tree

--- R code.R
+++ R code.R
@@ -46,3 +46,35 @@ data <- data.frame(
 )
 # check and print the dataframe
 print(data)
+
+## 1.9 hw
+# create a hypothetical file tree
+# Define the file tree structure
+# for R, data, and docs section, I reduced the file number into what I have on my computer
+# for other sections, I keep these hypothetical files which I do not have for now, but I will have later on
+file_tree <- "
+/OCD
+|-- R/
+|   |-- R code.R
+|-- data/
+|   |-- ocd_patient_dataset.csv
+|-- docs/
+|   |-- README.md
+|-- plots/
+|   |-- OCD patients information analysis/
+|       |-- plot1.png
+|       |-- plot2.png
+|-- results/
+|   |-- OCD patients information analysis results.txt
+|-- tests/
+|   |-- test_script1.R
+|   |-- test_script2.R
+|-- .gitignore
+|-- OCD.Rproj
+"
+# Specify the output file
+output_file <- "README.md"
+# Write the file tree to the README.md file
+writeLines(file_tree, con = output_file)
+# Print a message indicating success
+cat("File tree has been written to", output_file, "\n")
