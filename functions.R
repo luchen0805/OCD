2243902065a382058b1a5c5650829bf78c1b028e
Luchen Yu <155499840+luchen0805@users.noreply.github.com>
2024-01-12 09:40:56 -0600

    create hello_world function

---
+++ functions.R
@@ -0,0 +1,20 @@
+options(repos=c(CRAN="https://cran.rstudio.com"))
+# create a hello_world function
+snow <- TRUE
+
+hello_world <- function(day, weather) {
+  if (day == "Tuesday" && weather == TRUE) { # if it is Tuesday and it snows
+    "I will take the bus to the school."
+  } else if (day == "Tuesday" && weather == FALSE) { # if it is Tuesday and it dose not snow
+    "I will walk to school."
+  } else if (day != "Tuesday" && weather == TRUE) { # if it is not Tuesday and it snows
+    "I will stay at home."
+  } else { # if it is not Tuesday and it does not snow
+    "I will do grocery shopping."
+  }
+}
+
+hello_world(day = "Tuesday", weather = TRUE) # Tuesday and snow
+hello_world(day = "Tuesday", weather = FALSE) # Tuesday and no snow
+hello_world(day = "Monday", weather = TRUE) # not Tuesday and snow
+hello_world(day = "Monday", weather = FALSE) # not Tuesday and no snow
